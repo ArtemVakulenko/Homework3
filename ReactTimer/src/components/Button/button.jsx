7ecfe1d9f390eff5bf30e23c275c7c9476d2fc7c
ArtemVakulenko <artemvakulenko42@gmail.com>
2021-06-19 22:49:37 +0300

    setup for react timer

---
+++ ReactTimer/src/components/Button/button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import StButton from './styled';
+
+function Button(props) {
+    const { onClick, value } = props;
+        return (
+            <StButton onClick={onClick}>{value}</StButton>
+        );
+}
+
+Button.propTypes = {
+    onClick: propTypes.func,
+    value: propTypes.string,
+};
+
+export default Button;
